f9d64b680590b969f6e4f2945ea7071c436ba2f3
Wendy <vageekmom@gmail.com>
2020-01-14 09:31:02 -0500

    Shooter subsystem with joystick control.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import static frc.robot.drivetrain.Drivetrain.getDrivetrain;
+import static frc.robot.shooter.Shooter.getShooter;
 import static frc.robot.oi.OI.getOI;
 
 import com.team2363.logger.HelixEvents;
@@ -51,6 +52,7 @@ public class Robot extends TimedRobot {
   private void initializeSubsystems() {
     getOI();
     getDrivetrain();
+    getShooter();
   }
 
   /**
---
+++ src/main/java/frc/robot/shooter/Shooter.java
@@ -0,0 +1,139 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.shooter;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import frc.robot.oi.OI;
+import frc.robot.shooter.commands.StopShooter;
+import frc.robot.shooter.commands.TestWithController;
+
+public class Shooter extends Subsystem {
+
+    private static Shooter INSTANCE = new Shooter();
+
+    /**
+     * @return the singleton instance of the Drivetrain subsystem
+     */
+    public static Shooter getShooter() {
+      if (INSTANCE == null) {
+        INSTANCE = new Shooter();
+      }
+      return INSTANCE;
+    }
+  
+    private static final int SHOOTER_ID = 1;
+    private static final double MAX_RPM = 5700;
+    private double setPoint = 0.0;
+
+    private CANSparkMax motor;
+    private CANPIDController pidController;
+    private CANEncoder encoder;
+    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+     
+    public Shooter() {
+
+        // initialize motor
+        motor = new CANSparkMax(SHOOTER_ID, MotorType.kBrushless);
+        motor.restoreFactoryDefaults();
+        pidController = motor.getPIDController();
+        encoder = motor.getEncoder();
+
+        // PID coefficients
+        kP = 5e-5;
+        kI = 1e-6;
+        kD = 0;
+        kIz = 0;
+        kFF = 0;
+        kMaxOutput = 1;
+        kMinOutput = -1;
+
+        // set PID coefficients
+        pidController.setP(kP);
+        pidController.setI(kI);
+        pidController.setD(kD);
+        pidController.setIZone(kIz);
+        pidController.setFF(kFF);
+        pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+        // display PID coefficients on SmartDashboard
+        SmartDashboard.putNumber("P Gain", kP);
+        SmartDashboard.putNumber("I Gain", kI);
+        SmartDashboard.putNumber("D Gain", kD);
+        SmartDashboard.putNumber("I Zone", kIz);
+        SmartDashboard.putNumber("Feed Forward", kFF);
+        SmartDashboard.putNumber("Max Output", kMaxOutput);
+        SmartDashboard.putNumber("Min Output", kMinOutput);
+    }
+
+    @Override
+    public void periodic() {
+        
+        // read PID coefficients from SmartDashboard
+        double p = SmartDashboard.getNumber("P Gain", 0);
+        double i = SmartDashboard.getNumber("I Gain", 0);
+        double d = SmartDashboard.getNumber("D Gain", 0);
+        double iz = SmartDashboard.getNumber("I Zone", 0);
+        double ff = SmartDashboard.getNumber("Feed Forward", 0);
+        double max = SmartDashboard.getNumber("Max Output", 0);
+        double min = SmartDashboard.getNumber("Min Output", 0);
+
+        // if PID coefficients on SmartDashboard have changed, write new values to
+        // controller
+        if ((p != kP)) {
+            pidController.setP(p);
+            kP = p;
+        }
+        if ((i != kI)) {
+            pidController.setI(i);
+            kI = i;
+        }
+        if ((d != kD)) {
+            pidController.setD(d);
+            kD = d;
+        }
+        if ((iz != kIz)) {
+            pidController.setIZone(iz);
+            kIz = iz;
+        }
+        if ((ff != kFF)) {
+            pidController.setFF(ff);
+            kFF = ff;
+        }
+        if ((max != kMaxOutput) || (min != kMinOutput)) {
+            pidController.setOutputRange(min, max);
+            kMinOutput = min;
+            kMaxOutput = max;
+        }
+    }
+
+    public void setFromThrottle(){
+        setPoint = OI.getOI().getThrottle() * MAX_RPM;
+        setVelocity(setPoint);
+    }
+
+    public void setVelocity(double setPoint) {
+        pidController.setReference(setPoint, ControlType.kVelocity);
+
+        SmartDashboard.putNumber("SetPoint", setPoint);
+        SmartDashboard.putNumber("ProcessVariable", encoder.getVelocity());
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+        // setDefaultCommand(new TestWithController());
+        setDefaultCommand(new StopShooter());
+    }
+}
