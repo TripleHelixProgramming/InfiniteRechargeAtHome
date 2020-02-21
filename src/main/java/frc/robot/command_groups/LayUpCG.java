841a17e2b4a430157602d7ff51fd8c6342bec697
Wendy <vageekmom@gmail.com>
2020-02-21 13:04:43 -0500

    Adde LayupCG to backup 18 inches &spin up shooter.

---
+++ src/main/java/frc/robot/command_groups/LayUpCG.java
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.command_groups;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.paths.EightteenInchesBack;
+import frc.robot.drivetrain.commands.PathFollower;
+import frc.robot.shooter.Position;
+import frc.robot.shooter.commands.SpinShooterUp;
+
+public class LayUpCG extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public LayUpCG() {
+    addParallel(new PathFollower(new EightteenInchesBack()));
+    addParallel(new SpinShooterUp(Position.LAYUP_SHOOT));
+  }
+}
