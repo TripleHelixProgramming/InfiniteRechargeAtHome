e06216623aa8feab036f4a551eac16e24d6a27f0
nlaverdure <nate.laverdure@gmail.com>
2021-02-27 14:57:38 -0500

    revert unnecessary change to intake

--- src/main/java/frc/robot/intake/commands/DeployIntake.java
+++ src/main/java/frc/robot/intake/commands/DeployIntake.java
@@ -39,26 +39,23 @@ public class DeployIntake extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return true;
+    return false;
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
-    //HelixEvents.getInstance().addEvent("INTAKE", "Ending DeployIntake");
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
-    // Default Command will retract and stop Intake
   }
 
   protected Command parallelCommand() {
